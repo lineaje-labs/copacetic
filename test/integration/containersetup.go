4962adc4015cdf1cc2ca9e04e3fc421b18fea6ba
sachi097 <nieesachi.097@gmail.com>
2025-07-07 21:56:06 +0000

    Integration tests for DPKG based images - Debian and Ubuntu

---
+++ test/integration/containersetup.go
@@ -0,0 +1,34 @@
+package integration
+
+import (
+	"context"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/testcontainers/testcontainers-go"
+)
+
+type testContainer struct {
+	testcontainers.Container
+	URI string
+}
+
+func setupTestContainer(ctx context.Context, testContainerName string) (*testContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image:      testContainerName,
+		Name:       testContainerName,
+		SkipReaper: true,
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+		Reuse:            true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Infof("Successfully setup %v image", testContainerName)
+
+	return &testContainer{Container: container}, nil
+}
